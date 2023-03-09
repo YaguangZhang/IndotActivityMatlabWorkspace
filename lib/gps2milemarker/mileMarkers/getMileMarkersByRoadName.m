7efdee0fcbb4d7659fb2162396dbed2e927b57e4
Yaguang Zhang <yaguangzhang@outlook.com>
2023-03-09 11:45:15 -0500

    Add option to order output MMs by mileasge.

--- lib/gps2milemarker/mileMarkers/getMileMarkersByRoadName.m
+++ lib/gps2milemarker/mileMarkers/getMileMarkersByRoadName.m
@@ -1,5 +1,5 @@
 function mileMarkers = getMileMarkersByRoadName( ...
-    roadName, indotMileMarkers)
+    roadName, indotMileMarkers, flagOrderMMByMile)
 % GETMILEMARKERSBYROADNAME Find all mile markers marked with the specified
 % road name from the INDOT mile marker database (2016).
 %
@@ -9,6 +9,9 @@ function mileMarkers = getMileMarkersByRoadName( ...
 %     "I" as Interstate, "T" as Toll and "U" as US (case insensitive).
 %   - indotMileMarkers
 %     Loaded INDOT mile marker database. Also works with part of it.
+%   - flagOrderMMByMile
+%     Optional. Default to false. Set this to be true to order the output
+%     mileMarkers by their mileage values (increasingly).
 %
 % Outputs:
 %   - mileMarkers
@@ -19,6 +22,10 @@ function mileMarkers = getMileMarkersByRoadName( ...
 %     Cell. The road name labels extracted from indotMileMarkers.
 %
 % Yaguang Zhang, Purdue, 02/02/2021
+
+if ~exist('flagOrderMMByMile', 'var')
+    flagOrderMMByMile = false;
+end
 
 if evalin('base', ...
         '~exist(''INDOT_MILE_MARKERS_ROADNAME_LABELS'',''var'')')
@@ -33,5 +40,19 @@ end
 mileMarkers = indotMileMarkers(...
     strcmpi(INDOT_MILE_MARKERS_ROADNAME_LABELS, roadName)...
     );
+
+if flagOrderMMByMile
+    numOfMMs = length(mileMarkers);
+    postNums = nan(numOfMMs, 1);
+
+    for idxMM = 1:numOfMMs
+        [~, postNums(idxMM)] ...
+            = getRoadNameFromMileMarker(mileMarkers(idxMM));
+    end
+
+    [~, indicesOrderedMMs] = sort(postNums, 'ascend');
+    mileMarkers = mileMarkers(indicesOrderedMMs);
+end
+
 end
 % EOF
